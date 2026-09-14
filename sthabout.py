882cc3a3836d2cabebc37ed68b0b7074bd593648
favcat <apextolya@gmail.com>
2026-09-14 09:11:10 +0300

    idespisetouchpad

---
+++ sthabout.py
@@ -0,0 +1,12 @@
+def main():
+    name = "Yaroslav"
+    surname = "Korolchuk"
+    group = "IT-31"
+    birth_year = 2009
+    print(f"Name: {name} {surname}")
+    print(f"Group: {group}")
+    print(f"Age in 2026: {2026 - birth_year}")
+    print("Favourite language: Python")
+
+
+main()
