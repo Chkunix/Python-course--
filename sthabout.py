0f03080de1b8fc0f089b9ccf6b70a92ba2f55594
favcat <apextolya@gmail.com>
2026-09-14 09:17:31 +0300

    brawl

--- sthabout.py
+++ sthabout.py
@@ -2,11 +2,14 @@ def main():
     name = "Yaroslav"
     surname = "Korolchuk"
     group = "IT-31"
+    brawl = "stars"
     birth_year = 2009
     print(f"Name: {name} {surname}")
     print(f"Group: {group}")
     print(f"Age in 2026: {2026 - birth_year}")
+    print(f"Favorite game: {brawl}")
     print("Favourite language: Python")
+    
 
 
 main()
